6087d228b0f0137e1ac8bece04e939569c8683bf
Rustam Giliaziev <gilaziev@gmail.com>
2024-12-31 04:46:33 +0100

    bump

---
+++ src/app.tsx
@@ -0,0 +1,79 @@
+import { useObj } from "../lib"
+
+class User {
+  constructor(
+    public name: string,
+    private _pets: Pet[] = [],
+  ) {}
+
+  get username() {
+    return this.name.toLowerCase().replace(" ", "-")
+  }
+
+  changeName(name: string) {
+    this.name = name
+  }
+
+  print() {
+    const pets = this._pets.map((pet) => pet.name).join(", ")
+    console.log(`Name: ${this.name}; Pets: ${pets}`)
+  }
+
+  addPet(pet: Pet) {
+    this._pets.push(pet)
+  }
+
+  get pets() {
+    return this._pets
+  }
+}
+
+class Pet {
+  constructor(public name: string) {}
+}
+
+export function App() {
+  const user = useObj(() => new User("John Doe", [new Pet("Fluffy")]))
+
+  return (
+    <div>
+      <h1>{user.name}</h1>
+      <p>{user.username}</p>
+
+      <button onClick={() => user.changeName("Jane Doe")}>
+        Change Name (method)
+      </button>
+
+      <button
+        onClick={() => {
+          user.name = "Alice Doe"
+        }}
+      >
+        Change Name (Direct)
+      </button>
+
+      <button onClick={() => user.print()}>Print</button>
+
+      <div>
+        <h2>Pets</h2>
+        <input
+          type="text"
+          placeholder="Pet Name"
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              e.preventDefault()
+              user.addPet(new Pet(e.currentTarget.value))
+              e.currentTarget.value = ""
+            }
+          }}
+        />
+
+        <ul>
+          {user.pets.map((pet, i) => (
+            <li key={i}>{pet.name}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+}
